be1a2c3f5b2f099bd95cd7a8e0c93168a4103f70
mainu007 <mainudev.co@gmail.com>
2020-04-28 11:37:56 +0600

    Updates

---
+++ precache-manifest.3c486ea3f7a90613bebcf3a6ec6617eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4877859d1934327cf569c787abc60008",
+    "url": "/reactSoppingItem/index.html"
+  },
+  {
+    "revision": "bbc1c7f23bb79b43240f",
+    "url": "/reactSoppingItem/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "308d2cb24f380efd94f2",
+    "url": "/reactSoppingItem/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bbc1c7f23bb79b43240f",
+    "url": "/reactSoppingItem/static/js/2.55e720fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactSoppingItem/static/js/2.55e720fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "308d2cb24f380efd94f2",
+    "url": "/reactSoppingItem/static/js/main.8da732d8.chunk.js"
+  },
+  {
+    "revision": "04fa6ee3e2527d97fd62",
+    "url": "/reactSoppingItem/static/js/runtime-main.297cc2b4.js"
+  }
+]);
